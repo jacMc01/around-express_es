f3e533b4d0d420293faa49b0d1028842cd1977db
Jacqueline Mckay H <jacquelinemc@outlook.es>
2023-01-23 18:46:38 -0600

    feat: checkout mongodb connection

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const { port = 3000 } = process.env;
 const app = express();
+const mongoose = require('mongoose');
+
+const MONGODB_URI = 'mongodb://localhost:27017/aroundb';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Conectado a MongoDB'));
 
 const usersAPI = require('./routes/users');
 const cardsAPI = require('./routes/cards');
